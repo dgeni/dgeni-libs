5a9d79acf492134472ac49d2670db77fa7e1a36a
Peter Bacon Darwin <pete@bacondarwin.com>
2015-11-26 10:49:53 +0000

    feat(log): Implement log utilities

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,7 +1,35 @@
-export function log(... args) {
-	console.log.apply(args);
-}
+export var log = {
+	silly: (...args) => console.log.apply(console, args),
+	debug: (...args) => console.log.apply(console, args),
+	info: (...args) => console.log.apply(console, args),
+	warn: (...args) => console.log.apply(console, args),
+	error: (...args) => console.log.apply(console, args)
+};
 
-export function createDocMessage(message : String, doc : any) {
-	return message;
-}
+export function createDocMessage(message : string, doc? : any, error? : Error) {
+    message = message || '';
+    if ( doc ) {
+      message += ' - doc';
+      var docIdentifier = doc.id || doc.name || doc.path;
+      if ( docIdentifier ) {
+        message += ' "' + docIdentifier + '"';
+      }
+      if ( doc.docType ) {
+        message += ' (' + doc.docType + ') ';
+      }
+      var filePath = doc.fileInfo && doc.fileInfo.relativePath;
+      if ( filePath ) {
+        message += ' - from file "' + filePath + '"';
+        if ( doc.startingLine ) {
+          message += ' - starting at line ' + doc.startingLine;
+        }
+        if ( doc.endingLine ) {
+         message += ', ending at line ' + doc.endingLine;
+        }
+      }
+    }
+    if ( error ) {
+      message += '\n\nOriginal Error: \n\n' + error.stack;
+    }
+    return message;
+  }
